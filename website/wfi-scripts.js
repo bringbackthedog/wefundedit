2a8173c220ca5b895887b2e75807df54e979474a
Lenz Paul <lenz@pop-os.localdomain>
2020-11-09 14:50:10 -0800

    wip rework fixed sized px

---
+++ website/wfi-scripts.js
@@ -0,0 +1,29 @@
+// function myFunction() {
+//     var navbarElement = document.getElementById("wfi-navbar");
+//     navbarElement.classList.add("navbar-expand-lg");
+
+
+
+// function myFunction(x) {
+//   if (x.matches) { // If media query matches
+//     document.body.style.backgroundColor = "yellow";
+//   } else {
+//    document.body.style.backgroundColor = "pink";
+//   }
+// }
+
+// var x = window.matchMedia("(max-width: 700px)")
+// myFunction(x) // Call listener function at run time
+// // x.addListener(myFunction) // Attach listener function on state changes
+
+
+// const mql = window.matchMedia("(max-width: 1024x)");
+
+// mql.addEventListener("change", () => {
+//     this.checkNative();
+// });
+
+
+
+//  }
+ 
